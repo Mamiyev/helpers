4d356d940057917ea4a21b21f1b20dd3f6d81f94
Mamiyev <yuska19.99@mail.ru>
2022-06-04 22:56:46 +0300

    added Tag component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Button from "../helpers/uikit/button/Button";
+import Tag from "../helpers/uikit/tag/Tag";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
